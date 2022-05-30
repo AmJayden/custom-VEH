320f7411e718276eff9408a51594bafac63ebec1
Jayden <53480580+AmJayden@users.noreply.github.com>
2022-05-30 11:55:50 -0400

    Update custom_veh.hpp

--- custom handlers/veh/custom_veh.hpp
+++ custom handlers/veh/custom_veh.hpp
@@ -1,16 +1,17 @@
 #pragma once
 #include <deque>
 #include <cstdint>
+
 #include <Windows.h>
 
-namespace custom_handlers::veh
+namespace custom_veh
 {
-	using vectored_handler_t = std::uint32_t(*)(EXCEPTION_POINTERS*);
+	using vectored_handler_t = std::uint32_t( * )( EXCEPTION_POINTERS* );
+	
+	static std::deque< vectored_handler_t > g_vectored_handlers;
+	
+	bool add_vectored_handler( vectored_handler_t handler, bool first = true );
+	bool remove_vectored_handler( vectored_handler_t handler );
 
-	static std::deque<vectored_handler_t> g_vectored_handlers;
-
-	bool add_vectored_handler(vectored_handler_t func, bool first = true);
-	bool remove_vectored_handler(vectored_handler_t func);
-
-	bool start_custom_handler();
+	bool start_veh( );
 }
